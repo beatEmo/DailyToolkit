5907131b693e325ea3dbccb69b3fd8ecd834ffc6
wzy <wzyzzqq@163.com>
2024-08-11 21:15:12 +0800

    feat(base-components-cra): 封装Icon组件

--- packages/base-components-cra/src/App.tsx
+++ packages/base-components-cra/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { IconAdd } from "./Icon/icons/IconAdd";
+// import { IconEmail } from "./Icon/icons/IconEmail";
+
+// function App() {
+//   return (
+//     <div style={{ padding: "50px" }}>
+//       <IconAdd size={"64px"}></IconAdd>
+//       <IconEmail spin></IconEmail>
+//       <IconEmail style={{ color: "red", fontSize: "32px" }}></IconEmail>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { createFromIconfont } from "./Icon/createFrontIconfont";
+import { IconAdd } from "./Icon/icons/IconAdd";
+import { IconEmail } from "./Icon/icons/IconEmail";
+
+const IconFont = createFromIconfont(
+  "//at.alicdn.com/t/c/font_4443338_a2wwqhorbk4.js"
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ padding: "50px" }}>
+        <IconFont type="icon-shouye-zhihui" size="40px"></IconFont>
+        <IconFont
+          type="icon-gerenzhongxin-zhihui"
+          fill="blue"
+          size="40px"
+        ></IconFont>
+      </div>
     </div>
   );
 }
